c79a3888e789bf7484f1e1d413bdc557b5cebc6b
Simon Douglas <simondouglas@Simons-MBP.lan>
2017-07-08 14:02:40 +0100

    add list view and adapter

--- app/src/main/java/com/example/simondouglas/darkskyapp/MainActivity.java
+++ app/src/main/java/com/example/simondouglas/darkskyapp/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.simondouglas.darkskyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GenerateMockData mockData = new GenerateMockData();
+        ArrayList<WeatherData> weatherDataList = mockData.generateData();
+
+        WeatherDataAdapter weatherDataAdapter = new WeatherDataAdapter(this, weatherDataList);
+        ListView weatherList = (ListView) findViewById(R.id.weatherListView);
+
+        weatherList.setAdapter(weatherDataAdapter);
     }
 }
---
+++ app/src/main/java/com/example/simondouglas/darkskyapp/WeatherDataAdapter.java
@@ -0,0 +1,50 @@
+package com.example.simondouglas.darkskyapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by simondouglas on 08/07/2017.
+ */
+
+class WeatherDataAdapter extends ArrayAdapter<WeatherData> {
+
+    public WeatherDataAdapter(Context context, ArrayList<WeatherData> weatherDataList) {
+        super(context, 0, weatherDataList);
+    }
+
+    @Override
+    public View getView(int poisition, View weatherItemView, ViewGroup parent) {
+
+        if(weatherItemView == null) {
+            weatherItemView = LayoutInflater.from(getContext()).inflate(R.layout.weather_items, parent, false);
+        }
+
+        WeatherData currentData = getItem(poisition);
+
+        TextView timeView = (TextView) weatherItemView.findViewById(R.id.timeView);
+        ImageView weatherIconView = (ImageView) weatherItemView.findViewById(R.id.weatherIconView);
+        TextView summaryView = (TextView) weatherItemView.findViewById(R.id.summaryView);
+        TextView tempView = (TextView) weatherItemView.findViewById(R.id.temperatureView);
+
+        Date date = new Date(Long.parseLong(currentData.getTime()));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("H:mm");
+
+
+        timeView.setText(dateFormat.format(date));
+        summaryView.setText(currentData.getSummary());
+        tempView.setText(currentData.getTemperature());
+
+        return weatherItemView;
+    }
+
+}
