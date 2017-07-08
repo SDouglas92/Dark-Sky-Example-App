3471d92b2c925af95539312d052f179e63592ae2
Simon Douglas <simondouglas@Simons-MBP.lan>
2017-07-08 14:23:40 +0100

    add oncreate to activity details and create empty layout resource

--- app/src/main/java/com/example/simondouglas/darkskyapp/DetailsActivity.java
+++ app/src/main/java/com/example/simondouglas/darkskyapp/DetailsActivity.java
@@ -1,10 +1,22 @@
 package com.example.simondouglas.darkskyapp;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by simondouglas on 08/07/2017.
  */
 
-class DetailsActivity extends AppCompatActivity{
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        Intent intent = getIntent();
+        WeatherData weatherData = (WeatherData) intent.getSerializableExtra("weatherData");
+    }
+
 }
