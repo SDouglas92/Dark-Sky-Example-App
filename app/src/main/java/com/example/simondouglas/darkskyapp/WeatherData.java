02d7b14931fec5a673fc59b1538af37634438a00
Simon Douglas <simondouglas@Simons-MBP.lan>
2017-07-08 12:49:27 +0100

    Add get time method and mock data for test

--- app/src/main/java/com/example/simondouglas/darkskyapp/WeatherData.java
+++ app/src/main/java/com/example/simondouglas/darkskyapp/WeatherData.java
@@ -1,8 +1,20 @@
 package com.example.simondouglas.darkskyapp;
 
+import java.util.HashMap;
+
 /**
  * Created by simondouglas on 08/07/2017.
  */
 
 class WeatherData {
+    
+    private String time;
+    
+    public WeatherData(HashMap<String, String> data) {
+        time = data.get("time");
+    }
+
+    public String getTime() {
+        return time;
+    }
 }
--- app/src/test/java/com/example/simondouglas/darkskyapp/WeatherDataTest.java
+++ app/src/test/java/com/example/simondouglas/darkskyapp/WeatherDataTest.java
@@ -1,6 +1,11 @@
 package com.example.simondouglas.darkskyapp;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 /**
@@ -9,8 +14,39 @@ import static org.junit.Assert.*;
 
 public class WeatherDataTest {
 
+    WeatherData weatherData;
+    private HashMap<String, String> data1 = new HashMap<String, String>(){{
+        put("time", "1499515200");
+        put("summary", "Partly Cloudy");
+        put("icon", "partly-cloudy-day");
+        put("precipIntensity", "0");
+        put("precipProbability", "0");
+        put("temperature", "14.34");
+        put("apparentTemperature", "14.34");
+        put("dewPoint", "9.12");
+        put("humidity", "0.71");
+        put("windSpeed", "10.45");
+        put("windGust", "12.37");
+        put("windBearing", "328");
+        put("cloudCover", "0.3");
+        put("pressure", "1017.38");
+        put("ozone", "340.42");
+        put("uvIndex", "5");
+    }};
+
+    @Before
+    public void before() {
+        weatherData = new WeatherData(data1);
+    }
+
+
     @Test
     public void canCreateWeatherDataClass() {
-        assertNotNull(new WeatherData());
+        assertNotNull(weatherData);
+    }
+
+    @Test
+    public void canGetTime() {
+        assertEquals(data1.get("time"), weatherData.getTime());
     }
 }
